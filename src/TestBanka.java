7363ed44a43ad85af159e483bbe9e44921d9e850
Adri-Hadri <adriana.redep.matosevic@gmail.com>
2021-10-11 12:09:07 +0200

    Ovo nije dobro.

---
+++ src/TestBanka.java
@@ -0,0 +1,57 @@
+public class TestBanka {
+
+
+        private int idNumber;
+        private String first_name;
+        private int year;
+
+
+        public Student(int idNumber, String first_name, int year) {
+            this.idNumber = idNumber;
+            this.first_name = first_name;
+            this.year = year;
+        }
+
+        public Student() {
+
+        }
+
+        public int getIdNumber() {
+            return idNumber;
+        }
+
+        public String getFirst_name() {
+            return first_name;
+        }
+
+        public int getYear() {
+            return year;
+        }
+
+        public void setIdNumber(int idNumber) {
+            this.idNumber = idNumber;
+        }
+
+        public void IncreaseYear() {
+            year++;
+        }
+
+        @Override
+        public String toString() {
+            return "Student: {" +
+                    "idNumber= " + idNumber +
+                    ", first_name= '" + first_name + '\'' +
+                    ", year= " + year +
+                    '}' + "\n";
+        }
+
+        public void setFirst_name(String first_name) {
+            this.first_name = first_name;
+        }
+
+        public void setYear(int year) {
+            this.year = year;
+        }
+    }
+
+}
